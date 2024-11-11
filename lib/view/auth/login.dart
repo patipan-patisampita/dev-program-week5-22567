5acd105e5e5debbb640da027bc8150a1612ebeba
patipan-patisampita <patipan-patisampita@gmail.com>
2024-11-11 10:55:31 +0700

    update

---
+++ lib/view/auth/login.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+class LoginScreens extends StatefulWidget {
+  const LoginScreens({super.key});
+
+  @override
+  State<LoginScreens> createState() => _LoginScreensState();
+}
+
+class _LoginScreensState extends State<LoginScreens> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(8.0),
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // ===== Images Login =====
+                Image.asset(
+                  'assets/images/login.png',
+                  // width: size.width * 0.9,
+                  width: size.width - 100,
+                ),
+                // ===== Text Login =====
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Text(
+                        'Login',
+                        style: TextStyle(
+                          fontSize: 30.0,
+                          color: Colors.lightBlue,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                // ===== Form Login =====
+                Form(
+                  child: Column(
+                    children: [
+                      // ===== Enter field Email =====
+                      const TextField(),
+                      const SizedBox(height: 10.0),
+                      // ===== Enter field password =====
+                      const TextField(),
+                      const SizedBox(height: 10.0),
+                      // ===== Button submit login =====
+                      InkWell(
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                          height: 55.0,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            color: Colors.lightBlue,
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                          child: const Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.lock_open, color: Colors.white),
+                              SizedBox(width: 5.0),
+                              Text(
+                                "Sign In",
+                                style: TextStyle(
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold,
+                                  wordSpacing: 2,
+                                  color: Colors.white
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                // ===== Button Link Register =====
+                const Row(
+                  children: [
+                    Text("Don't have an account?"),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
